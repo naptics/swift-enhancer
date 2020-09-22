8f3321d47ab3786a5da4a739cbd00075aafb4873
seeppp <jonas.schoch@naptics.ch>
2020-09-22 16:02:43 +0200

    update for linting

--- Tests/SwiftEnhancerTests/StringTests.swift
+++ Tests/SwiftEnhancerTests/StringTests.swift
@@ -1,5 +1,5 @@
-import XCTest
 @testable import SwiftEnhancer
+import XCTest
 
 final class StringTests: XCTestCase {
     func testNilOrEmpty() {
--- Tests/SwiftEnhancerTests/XCTestManifests.swift
+++ Tests/SwiftEnhancerTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
 import XCTest
 
 #if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(SwiftEnhancerTests.allTests),
-    ]
-}
+    public func allTests() -> [XCTestCaseEntry] {
+        [
+            testCase(SwiftEnhancerTests.allTests),
+        ]
+    }
 #endif
