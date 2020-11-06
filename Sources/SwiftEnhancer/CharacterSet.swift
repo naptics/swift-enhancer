dfe34ce49572bbc5022df4d9a22261adba68e2f5
seeppp <jonas.schoch@naptics.ch>
2020-11-06 09:47:57 +0100

    reformat

--- Sources/SwiftEnhancer/CharacterSet.swift
+++ Sources/SwiftEnhancer/CharacterSet.swift
@@ -1,9 +1,8 @@
 import Foundation
 
-extension CharacterSet {
+public extension CharacterSet {
     /// Returns the character set for characters unreserved for URI components.
     ///
     /// see: https://tools.ietf.org/html/rfc3986#section-2.3
-    public static let rfc3986Unreserved = CharacterSet.alphanumerics.union(.init(charactersIn: "-._~"))
+    static let rfc3986Unreserved = CharacterSet.alphanumerics.union(.init(charactersIn: "-._~"))
 }
-
--- Sources/SwiftEnhancer/Sequence.swift
+++ Sources/SwiftEnhancer/Sequence.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-extension Sequence {
-    public func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> [Element] {
+public extension Sequence {
+    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> [Element] {
         sorted { $0[keyPath: keyPath] < $1[keyPath: keyPath] }
     }
 }
